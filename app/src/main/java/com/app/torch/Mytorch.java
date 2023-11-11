cb914be63dea8ae3845de33a55db59d472e04f82
Pruthiraj Routaray <113031912+pruthiraj-74@users.noreply.github.com>
2023-11-11 19:14:15 +0530

    Initial commit

---
+++ app/src/main/java/com/app/torch/Mytorch.java
@@ -0,0 +1,36 @@
+package com.app.torch;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+
+public class Mytorch extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mytorch);
+        views.setImageViewResource(R.id.appwidget_image, R.drawable.flashlight);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
